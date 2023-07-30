1bf2032fffa2f799855a289ef4ad131454b87cee
unknown <samolazov.roman.alex@gmail.com>
2023-07-30 11:58:41 +0300

    ADD adaptive and flexibility

---
+++ build/scripts/media.js
@@ -0,0 +1,6 @@
+// const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-items');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
